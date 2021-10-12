0a204817b20836e754ababa6472c2d049de4d69c
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-10-12 01:49:00 -0500

    Add Heroku

---
+++ server.js
@@ -0,0 +1,62 @@
+// load the express module
+const express = require('express');
+
+// requiring the animals data 
+const {animals} = require('./data/animals');
+
+// instantiating the server 
+const app = express();
+
+// filtering functionality through the animals, returning a new filtered array 
+function filterByQuery(query, animalsArray){
+    let personalityTraitsArray = [];
+    // saved animalsArray as filteredResults
+    let filteredResults = animalsArray;
+    if(query.personalityTraits) {
+        // save personalityTraits as a dedicated array.
+        // if personalityTraits is a string, place it into a new array and save 
+        if(typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        // loop through each trait in the array
+        personalityTraitsArray.forEach(trait => {
+            // Check the trait against each animal in the filteredResults array.
+            // Remember, it is initially a copy of the animalsArray,
+            // but here we're updating it for each trait in the .forEach() loop.
+            // For each trait being targeted by the filter, the filteredResults
+            // array will then contain only the entries that contain the trait,
+            // so at the end we'll have an array of animals that have every one 
+            // of the traits when the .forEach() loop is finished.
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+
+    if(query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if(query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if(query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+// adding the route
+app.get('/api/animals', (req,res) => {
+    let results = animals;
+    if(req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+// make the server listen for requests
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
