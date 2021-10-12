22e855acf8e5194d375c078b92387e66e3208c4d
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-10-12 02:49:36 -0500

    created a web server with heroku

--- server.js
+++ server.js
@@ -59,6 +59,23 @@ app.get('/api/animals', (req,res) => {
     res.json(results);
 });
 
+// function that returns a single animal object
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+// adding the :id to a new route using req.params
+// a param route must always come after the other GET route
+app.get('/api/animals/:id', (req,res) => {
+    const result = findById(req.params.id, animals);
+    if(result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
 // make the server listen for requests
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
